bfec61b704d63e8407638e7b451aea500a9567e9
Haecker <120182849+Null1234567891011@users.noreply.github.com>
2025-05-10 17:40:15 +0300

    HowToUse.lua
    Use This Example in Roblox.

---
+++ HowToUse.lua
@@ -0,0 +1,16 @@
+loadstring(game:HttpGet(https://raw.githubusercontent.com/Null1234567891011/Xmodule/refs/heads/main/Xmodule.lua))()
+
+--Example for Module Script
+Modules.Xmodule.createModule("HelloWorld")
+
+local function printHW()
+  print("Hello, World!")
+end
+
+Modules.Xmodule.HelloWorld.add(printHW, "MyFunctionName")
+
+--Example for Script
+Modules.HelloWorld.MyFunctionName()
+
+--Log:
+Hello, World!
