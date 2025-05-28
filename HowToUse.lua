9cf4bd9c31e7a371383fbd75f8825d2a01de3e36
Haecker <120182849+Null1234567891011@users.noreply.github.com>
2025-05-28 13:05:35 +0300

    Update HowToUse.lua

--- HowToUse.lua
+++ HowToUse.lua
@@ -1,5 +1,5 @@
 --Example for Module Script
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Null1234567891011/Xmodule/refs/heads/main/Xmodule.lua"))() Modules=_G.Modules
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Null1234567891011/Xmodule/refs/heads/main/Xmodule.lua")) Modules=_G.Modules
 
 Modules.Xmodule.createModule("HelloWorld")
 
@@ -10,7 +10,7 @@ end
 Modules.Xmodule.HelloWorld.add(printHW, "MyFunctionName")
 
 --Example for Script
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Null1234567891011/Xmodule/refs/heads/main/Xmodule.lua"))() Modules=_G.Modules
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Null1234567891011/Xmodule/refs/heads/main/Xmodule.lua")) Modules=_G.Modules
 
 Modules.HelloWorld.MyFunctionName()
 
